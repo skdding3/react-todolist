4001884eba17a982c797031b54455f76eb1900c4
skdding3 <skdding3@gmail.com>
2022-05-17 23:05:42 +0900

    TodoList add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
 
 // 특정 컴포넌트가 아닌 GlobalStyle 적용
 
@@ -17,6 +18,7 @@ function App() {
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
+        <TodoList />
       </TodoTemplate>
     </>
   );
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+  // 사이즈 조정 확인
+  background: gray;
+`;
+
+function TodoList() {
+  return <TodoListBlock>TodoList</TodoListBlock>;
+}
+
+export default TodoList;
