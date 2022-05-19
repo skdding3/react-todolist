a3d0ae13b5cee53157ba9c5b4c8f5787e8a6b2d1
skdding3 <skdding3@gmail.com>
2022-05-19 18:06:17 +0900

    TodoItems

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItems from "./TodoItems";
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  // 사이즈 조정 확인
-  background: gray;
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItems text="극락 프로젝트" done={true} />
+      <TodoItems text="컴포넌트 스타일링" done={true} />
+      <TodoItems text="Context 만들기" done={false} />
+      <TodoItems text="기능 구현" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
