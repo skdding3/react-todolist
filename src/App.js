6d592b2e7e5e9180880900307c0e419484c6493b
skdding3 <skdding3@gmail.com>
2022-05-23 18:32:58 +0900

    TodoProviders

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
 import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 // 특정 컴포넌트가 아닌 GlobalStyle 적용
 
@@ -15,14 +16,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
